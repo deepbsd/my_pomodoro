1bd1ea3f2e885bef0870b8c56660250ec64e0bb8
David S. Jackson <deepbsd@yahoo.com>
2020-02-11 18:11:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#  My Pomodoro
+
+This is a simple shell script for displaying elapsed time as per
+a pomodoro time keeping program
+
+
---
+++ my_promodoro.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+short_break=5
+long_break=25
+pomodoro=25
+((durationinsecs=${pomodoro}*3600/60))
+
+
+start_time=$(date +%s)
+
+compute(){
+   now_time=$(date +%s)
+   elapsed=$( expr $now_time - $start_time )
+   printf "%d " "${elapsed}"
+}
+
+
+convertsecs(){
+    ((h=${1}/3600))
+    ((m=(${1}%3600)/60))
+    ((s=${1}%60))
+    printf "%02d:%02d:%02d" $h $m $s
+}
+
+remainingsecs(){
+    ((m=${pomodoro}-1-${1}%3600/60))
+    ((s=(60-${1}%60)))
+    printf "%02d:%02d" $m $s
+}
+
+
+show_pom(){
+
+clear
+
+cat <<EOF
+
+
+                    Welcome to MyPomodoro!
+
+        Short break: ${short_break}     Elapsed: ${elapsed}
+        Long break: ${long_break}       Remaining: ${remaining}
+
+
+
+EOF
+
+}
+
+while true; do
+    show_pom
+    pomo=$(compute)
+    elapsed=$(convertsecs $pomo)
+    remaining=$(remainingsecs $pomo)
+    if [ $pomo -gt $durationinsecs ]; then
+        break
+    fi
+    sleep 1
+done
+
+
+
+
